0fb8b4661173ff870f64896f7e4337d39def4bb1
Yusuf Giyaziddinov <ygiyaziddinov@gmail.com>
2021-03-15 13:41:14 +0500

    feat: test solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let arrA = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+    arrB = [ 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
+    arrC = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    
+    num = number + "";
+    let readable = [];
+    let numToArr = num.split("");
+
+    for (i = 0; i < numToArr.length; i++) {
+        if (num === "0") {
+            return "zero";
+        }
+        if (numToArr.length === 3) {
+            readable.push(arrA[numToArr[i] - 1]);
+            readable.push("hundred");
+            numToArr.shift();
+            num = num.substr(1);
+        }
+        if (
+            numToArr.length === 2 &&
+            numToArr[numToArr.length - 2] === "0" &&
+            numToArr[numToArr.length - 1] === "0"
+        ) {
+            return readable.join(" ");
+        }
+        if (numToArr.length === 1 || numToArr[numToArr.length - 2] === "0") {
+            readable.push(arrA[num - 1]);
+            return readable.join(" ");
+        }
+        if (numToArr.length === 2 && numToArr[numToArr.length - 1] === "0") {
+            readable.push(arrB[numToArr[i] - 1]);
+            return readable.join(" ");
+        }
+        if (num > 10 && num < 20) {
+            readable.push(arrC[numToArr[numToArr.length - 1] - 1]);
+            return readable.join(" ");
+        }
+        if (numToArr.length === 2) {
+            readable.push(arrB[numToArr[i] - 1]);
+            readable.push(arrA[numToArr[numToArr.length - 1] - 1]);
+            return readable.join(" ");
+        }
+    }
+};
